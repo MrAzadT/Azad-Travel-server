6bef756c51d6d4dd9af635eb8c3c2e069a952c71
azadtamal <mrazadt@gmail.com>
2021-10-29 20:26:28 +0600

    5%

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
 
+// const bodyParser = require("body-parser");
+// require("dotenv").config()
+// const ObjectId = require("mongodb").ObjectId;
+
 app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikemc.mongodb.net/azadTravel?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
