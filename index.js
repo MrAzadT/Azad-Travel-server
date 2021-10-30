373222f533e9f17601fca9fc24996a396db718e3
azadtamal <mrazadt@gmail.com>
2021-10-30 22:26:00 +0600

    40%

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
+
 const app = express();
 const port = process.env.PORT || 4000;
 
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 require("dotenv").config();
-const ObjectId = require("mongodb").ObjectId;
+// const ObjectId = require("mongodb").ObjectId;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikemc.mongodb.net/azadTravel?retryWrites=true&w=majority`;
 
@@ -19,19 +20,24 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-client.connect((err) => {
-  const ordersCollection = client.db("azadTravel").collection("users");
-  const dataCollection = client.db("azadTravel").collection("myData");
-  // perform actions on the collection object
-  // client.close();
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("azadTravel");
+    const dataCollection = database.collection("myData");
+    const userCollection = database.collection("users");
 
-  // get data
-  app.get("/myData", (req, res) => {
-    dataCollection.find({}).toArray((err, results) => {
-      res.send(results);
+    // get data
+    app.get("/myData", async (req, res) => {
+      const cursor = dataCollection.find({});
+      const data = await cursor.toArray();
+      res.send(data);
     });
-  });
-});
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
