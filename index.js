cf9e35b0eb4503e2530f58d59249fa3cb98d0f31
azadtamal <mrazadt@gmail.com>
2021-10-30 15:45:37 +0600

    20%

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 4000;
 
-// const bodyParser = require("body-parser");
-// require("dotenv").config()
-// const ObjectId = require("mongodb").ObjectId;
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ikemc.mongodb.net/azadTravel?retryWrites=true&w=majority`;
 
@@ -19,9 +20,17 @@ const client = new MongoClient(uri, {
 });
 
 client.connect((err) => {
-  const collection = client.db("test").collection("devices");
+  const ordersCollection = client.db("azadTravel").collection("users");
+  const dataCollection = client.db("azadTravel").collection("myData");
   // perform actions on the collection object
   // client.close();
+
+  // get data
+  app.get("/myData", (req, res) => {
+    dataCollection.find({}).toArray((err, results) => {
+      res.send(results);
+    });
+  });
 });
 
 app.get("/", (req, res) => {
